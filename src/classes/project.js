06042bee7a9b490f440092853e14d3f1783c6818
bofmar <bofmar@outlook.com>
2022-08-17 12:54:39 +0300

    Add skeleton for Project object

---
+++ src/classes/project.js
@@ -0,0 +1,49 @@
+export default class Project{
+  #title;
+  #itemsList;
+  #description;
+  #id;
+
+  constructor(title,itemList,description){
+    this.#title = title;
+    this.#itemsList = itemList || [];
+    this.#description = description;
+    this.#id = crypto.randomUUID();
+  }
+
+  // geters
+  getTitle(){
+    return this.#title;
+  }
+  getItemsList(){
+    return this.#itemsList;
+  }
+  getDescription(){
+    return this.#description;
+  }
+
+  // seters
+  setTitle(newTitle){
+    this.#title = newTitle;
+  }
+  setDescription(newDescription){
+    this.#description = newDescription;
+  }
+
+  length(){
+    return this.#itemsList.length;
+  }
+  addItem(item){
+    this.#itemsList.push(item);
+  }
+  removeItem(item){
+    const index = this.#itemsList.findIndex(toDoItem => toDoItem.getID() === item.getID()); // find the index of the element we want to remove
+    if(index === -1) return; // see if we found the element
+    this.#itemsList.splice(index,1);
+  }
+  readItem(item){
+    const index = this.#itemsList.findIndex(toDoItem => toDoItem.getID() === item.getID());
+    if(index === -1) return; // see if we found the element
+    return this.getItemsList()[index];
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import toDoItem from "./classes/todo.js";
+import Project from "./classes/project.js";
 
 const testItem = new toDoItem("Make cake","1/1/22","Making cakes is awesome", "Low", false);
 
@@ -8,5 +9,43 @@ testItem.setTitle("Make cake 2");
 console.log(testItem.getTitle());
 testItem.flipDone();
 console.log(testItem.isDone());
+console.log(testItem.getID());
 
-console.log(JSON.stringify(testItem));
+const myProject = new Project("Has values",[testItem],"New description");
+const emptyProject = new Project("Has no values", null, "Has a description");
+
+myProject.setTitle("Has one value");
+console.table(myProject);
+console.log(myProject.getDescription());
+
+
+console.table(emptyProject);
+console.log(emptyProject.length());
+emptyProject.addItem(testItem);
+console.table(emptyProject);
+console.log(emptyProject.getItemsList());
+
+
+const fakeObject = new toDoItem("Make cake33333","1/1/22","Making cakes is awesome", "Low", false);
+const newObject1 = new toDoItem("Make cake3","1/1/22","Making cakes is awesome", "Low", false);
+const newObject2 = new toDoItem("Make cake4","1/1/22","Making cakes is awesome", "Low", false);
+const newObject3 = new toDoItem("Make cake5","1/1/22","Making cakes is awesome", "Low", false);
+const newObject4 = new toDoItem("Make cake6","1/1/22","Making cakes is awesome", "Low", false);
+
+emptyProject.addItem(newObject1);
+emptyProject.addItem(newObject2);
+emptyProject.addItem(newObject3);
+emptyProject.addItem(newObject4);
+
+console.clear();
+emptyProject.removeItem(fakeObject);
+console.table(emptyProject.getItemsList());
+
+
+emptyProject.removeItem(testItem);
+console.table(emptyProject.getItemsList());
+
+emptyProject.removeItem(newObject3);
+console.table(emptyProject.getItemsList());
+
+console.log(emptyProject.readItem(newObject2));
