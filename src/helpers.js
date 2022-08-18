23747228b761f2243f30148fcca542441e9bce83
bofmar <bofmar@outlook.com>
2022-08-18 09:51:32 +0300

    Add function that create an element and attaches a class to it in helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -3,3 +3,11 @@ export function createAndAppendText(type,target,message){
   text.innerText = message;
   target.appendChild(text);
 }
+
+export function createWithClass(type,className,message = ""){
+  const newElement = document.createElement(type);
+  newElement.classList.add(className);
+  newElement.innerText = message;
+
+  return newElement;
+}
