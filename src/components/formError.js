02cda03f2a4540839e91fdc293177422c03da1b6
bofmar <bofmar@outlook.com>
2022-08-22 10:11:35 +0300

    Add form validation error modal

---
+++ src/components/formError.js
@@ -0,0 +1,18 @@
+export default function formError(){
+  const modal = document.createElement("dialog");
+  modal.classList.add("modal", "form-error-modal");
+
+  const title = document.createElement("h3");
+  title.innerText = "Validation Error";
+  modal.appendChild(title);
+
+  const message = document.createElement("p");
+  message.innerText = "Please fill out the mandatory fields";
+  modal.appendChild(message);
+
+  const button = document.createElement("button");
+  button.innerText = "OK";
+  modal.appendChild(button);
+
+  return modal;
+}
