c4b303326d43974d43f548b806f3bbfc54f557dd
bofmar <bofmar@outlook.com>
2022-08-28 18:02:49 +0300

    Add elements to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,28 @@
 export default function footerComp(){
-  const temp = document.createElement("footer");
-  temp.innerText = "This is a placeholder for the footer";
+  const content = document.createElement("footer");
+  
+  const text = document.createElement("h3");
+  text.innerText = "Created by Marios Mpofilakis";
+  content.appendChild(text);
 
-  return temp;
+  const socials = document.createElement("div");
+  content.appendChild(socials);
+  
+  const gitHub = document.createElement("a");
+  gitHub.setAttribute("href","https://github.com/bofmar");
+  gitHub.setAttribute("target", "blank");
+  gitHub.innerText = "github";
+  socials.appendChild(gitHub);
+
+  const codePen = document.createElement("a");
+  codePen.setAttribute("href","https://codepen.io/bofmar");
+  codePen.innerText = "codePen";
+  socials.appendChild(codePen);
+
+  const linkedIn = document.createElement("a");
+  linkedIn.setAttribute("href", "https://www.linkedin.com/in/marios-mpofilakis-03a1651b6/");
+  linkedIn.innerText = "linkedin";
+  socials.appendChild(linkedIn);
+
+  return content;
 }
