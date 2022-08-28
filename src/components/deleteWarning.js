c29afee6f047fa830e6672320c674d7e556522eb
bofmar <bofmar@outlook.com>
2022-08-28 19:21:13 +0300

    Move cancel button infront of ok for uniformity in the ui

--- src/components/deleteWarning.js
+++ src/components/deleteWarning.js
@@ -10,15 +10,16 @@ export default function deleteWarning(project){
   message.innerText = "Please note that this is irreversible. The project and all tasks will be permanently deleted.";
   modal.appendChild(message);
 
-  const okButton = document.createElement("button");
-  okButton.innerText = "OK";
-  okButton.setAttribute("id","delete");
-  modal.appendChild(okButton);
-
+  
   const cancelButton = document.createElement("button");
   cancelButton.innerText = "Cancel";
   cancelButton.setAttribute("id","cancel");
   modal.appendChild(cancelButton);
+
+  const okButton = document.createElement("button");
+  okButton.innerText = "OK";
+  okButton.setAttribute("id","delete");
+  modal.appendChild(okButton);
 
   return modal;
 }
