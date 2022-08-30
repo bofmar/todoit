e1a0d8badb75c6cbedda37fb697e5f772ad2835d
bofmar <bofmar@outlook.com>
2022-08-30 12:02:04 +0300

    Lint the time file

--- src/time.js
+++ src/time.js
@@ -1,77 +1,74 @@
-import format from "date-fns/format";
-import compareDesc from "date-fns/compareDesc";
-import parseISO from "date-fns/parseISO";
+import format from 'date-fns/format';
+import compareDesc from 'date-fns/compareDesc';
+import parseISO from 'date-fns/parseISO';
 
-export function getToday(){
+export function getToday() {
   const currentDate = new Date();
-  const today = format(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()), "yyyy-MM-dd");
+  const today = format(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()), 'yyyy-MM-dd');
   return today;
 }
 
-export function getOneWeek(){
+export function getOneWeek() {
   const currentDate = new Date();
-  const nextWeek = format(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 6), "yyyy-MM-dd");
+  const nextWeek = format(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 6), 'yyyy-MM-dd');
   return nextWeek;
 }
 
-export function filterWithinWeek(items){
-  const result = items.filter(item => {
-    if(item.getDueDate() === "Never"){
+export function filterWithinWeek(items) {
+  const result = items.filter((item) => {
+    if (item.getDueDate() === 'Never') {
+      return false;
+    } if (compareDesc(parseISO(item.getDueDate()), parseISO(getOneWeek())) === -1
+        || item.isDone()) {
       return false;
     }
-    else if(compareDesc(parseISO(item.getDueDate()), parseISO(getOneWeek())) === -1 || item.isDone()){
-      return false;
-    }
-    else{
-      return true;
-    }
+    return true;
   });
   return result;
 }
 
-export function compare(a,b){
-  if(a.getDueDate() === "Never"){
+export function compare(a, b) {
+  if (a.getDueDate() === 'Never') {
     return -1;
   }
-  if(b.getDueDate() === "Never"){
+  if (b.getDueDate() === 'Never') {
     return 1;
   }
 
-  return compareDesc(parseISO(a.getDueDate()),parseISO(b.getDueDate()));
+  return compareDesc(parseISO(a.getDueDate()), parseISO(b.getDueDate()));
 }
 
-export function isExpired(a){
-  if(a.getDueDate() === "Never"){
+export function isExpired(a) {
+  if (a.getDueDate() === 'Never') {
     return false;
   }
-  else{
-    if(compareDesc(parseISO(a.getDueDate()), parseISO(getToday())) === -1 || compareDesc(parseISO(a.getDueDate()), parseISO(getToday())) === 0){
-      return false;
-    }
+
+  if (compareDesc(parseISO(a.getDueDate()), parseISO(getToday())) === -1
+    || compareDesc(parseISO(a.getDueDate()), parseISO(getToday())) === 0) {
+    return false;
+  }
+  return true;
+}
+
+export function expiresToday(a) {
+  if (a.getDueDate() === 'Never') {
+    return false;
+  }
+
+  if (compareDesc(parseISO(a.getDueDate()), parseISO(getToday())) === 0) {
     return true;
   }
+  return false;
 }
 
-export function expiresToday(a){
-  if(a.getDueDate() === "Never"){
+export function expiresInAWeek(a) {
+  if (a.getDueDate() === 'Never') {
     return false;
   }
-  else{
-    if(compareDesc(parseISO(a.getDueDate()), parseISO(getToday())) === 0){
-      return true;
-    }
-    return false;
-  }
-}
 
-export function expiresInAWeek(a){
-  if(a.getDueDate() === "Never"){
+  if (compareDesc(parseISO(a.getDueDate()), parseISO(getOneWeek())) === -1
+    || compareDesc(parseISO(a.getDueDate()), parseISO(getOneWeek())) === 0) {
     return false;
   }
-  else{
-    if(compareDesc(parseISO(a.getDueDate()), parseISO(getOneWeek())) === -1 || compareDesc(parseISO(a.getDueDate()), parseISO(getOneWeek())) === 0){
-      return false;
-    }
-    return true;
-  }
+  return true;
 }
